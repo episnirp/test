d625ab0718950598ddf98acab8c6ef7049ad3c7c
episnirp <156193874+episnirp@users.noreply.github.com>
2024-01-24 21:53:50 +0800

    Add files via upload

---
+++ question.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What are the Logarithmic Laws?",
+        answer: "C. Product, Quotient, Power",
+        options: [
+            "A. Product, Law, Identity",
+            "B. Quotient, Power, Inverse",
+            "C. Product, Quotient, Power",
+            "D.  Addition, Subtraction, Exponent"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Simplify the following expression using the Quotient Rule: log5 (125) - log5 (5)",
+        answer: "D.  2",
+        options: [
+            "A. 130",
+            "B. 625",
+            "C. 120",
+            "D.  2"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Logarithm to Exponential: log2(8)=3",
+        answer: "A. 2^3 = 8",
+        options: [
+            "A. 2^3 = 8",
+            "B. 3^2 = 8",
+            "C. 8^2 = 3",
+            "D. 2^8 = 3"
+        ]
+    },
+    {
+        numb: 4,
+        question:"Logarithm to Exponential: log3(81) = 4",
+        answer: "B. 3^4 = 81",
+        options: [
+            "A. 81^4 = 3",
+            "B. 3^4 = 81",
+            "C. 81^4 = 4",
+            "D. 4^3 = 81",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Exponential to Logarithm: 3^2 = 9",
+        answer: "D. log3 (9) = 2",
+        options: [
+            "A. log3 (2) = 9",
+            "B. log2 (3) = 9",
+            "C. log9 (2) = 3",
+            "D. log3 (9) = 2"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Exponential to Logarithm: 10^1 = 10",
+        answer: "A. log(10) = 1",
+        options: [
+            "A. log(10) = 1",
+            "B. log1 (10) = 10",
+            "C. log10 (1) = 10",
+            "D. 1log = 10"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Solve for the x in: 2log7(x) = log7(2) + log7(x+12)",
+        answer: "B. 6",
+        options: [
+            "A. -6 and 4",
+            "B. 6",
+            "C. 6 and -4",
+            "D. -4"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Identify whether ln(x) = x - sqrt4,  is a Logarithmic Equation or Not.",
+        answer: "D. Logarithmic Equation.",
+        options: [
+            "A. Logarithmic Expression",
+            "B. Logarithmic Inequality.",
+            "C. Logarithmic Function",
+            "D. Logarithmic Equation."
+        ]
+    },
+    {
+        numb: 9,
+        question: "Suppose that an 2 earthquakes happened in close proximity underwater. One released an energy output of 10^6, and the other released an energy output of 10^3. What is the magnitude of the first earthquake compared to the second earthquake? Use the formula: log(E1/E2)",
+        answer: "A. 3",
+        options: [
+            "A. 3",
+            "B. 1",
+            "C. 6",
+            "D. 9"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Hana invested 15,000PHP in her friend's company. Her money doubled into 30,000PHP annually. With a rate of 2%, how long did it take for Hana's money to double? Use the formula P(t) = P(1+r)^t.",
+        answer: "C. 35 years",
+        options: [
+            "A. 70 years",
+            "B. 2 years",
+            "C. 35 years" ,
+            "D. 50 years"
+        ]
+    },
+];
